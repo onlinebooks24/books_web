5e8e5caf0489b1f8c3d72619340bd9571670355b
Mashpy <mashpysays@gmail.com>
2017-05-14 19:36:55 +0600

    used span instead of div

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -5,7 +5,7 @@ namespace App\Helpers;
 class Helper
 {
     public static function readMoreHelper($string) {
-        $new_short_string = substr($string, 0, strpos($string, '<div class="hidden">read more</div>'));
+        $new_short_string = substr($string, 0, strpos($string, '<span class="hidden">read more</span>'));
 
         if(!empty($new_short_string)){
             $string = $new_short_string;
