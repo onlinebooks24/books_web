4a477a3127d41cc38990b18ef34fbd836979e608
Mashpy <mashpysays@gmail.com>
2017-06-11 19:48:21 +0600

    Fixed www and non www problem

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -2,14 +2,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Redirect;
 
 class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
+        $request=app('request');
+        $host=$request->header('host');
         if(! $request->secure() && env('APP_ENV') != 'local') {
             return redirect()->secure($request->path());
         }
+
+        if (substr($host, 0, 4) != 'www.') {
+            $request->headers->set('host', 'www.'.$host);
+            return Redirect::to($request->path());
+        }
+
         return $next($request);
     }
 }
