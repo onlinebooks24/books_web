0dfa4f2ae12a391efb888c066ad9d29f4d365880
Mashpy <mashpysays@gmail.com>
2017-06-13 16:18:19 +0600

    disable google analytics for production

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -29,16 +29,17 @@
     <script type="text/javascript" src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/bootstrap.min.js')}}"></script>
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    @if (env('APP_ENV') == 'production' && empty(Auth::user()))
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-97703152-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
+            ga('create', 'UA-97703152-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    @endif
 
     @yield('run_custom_js_file')
     @yield('run_custom_jquery')
