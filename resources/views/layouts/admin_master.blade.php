1f13c926b6747d5b958f29600ab10127eaca3723
Mashpy <mashpysays@gmail.com>
2018-01-11 22:28:48 +0600

    Fix admin frontend

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -7,10 +7,10 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width">
   <link rel="shortcut icon" href="/favicon.ico">
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-    <script type="text/javascript" src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js')}}"></script>
+  <link rel="stylesheet" type="text/css" href="{{ asset('asset_admin/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('asset_admin/css/style.css') }}">
+    <script type="text/javascript" src="{{ asset('asset_admin/js/jquery-2.1.4.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('asset_admin/js/bootstrap.min.js')}}"></script>
     @yield('run_emergency_js')
     @yield('run_custom_css_file')
     @yield('run_custom_css')
