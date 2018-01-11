c657dc7f0958ba5ade48d5cf1468e10c17aef3cf
Mashpy <mashpysays@gmail.com>
2018-01-12 03:41:09 +0600

    Fixed javascript

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,7 +43,7 @@
 
 @yield('content')
 
-<script type="text/javascript" src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/bootstrap.min.js')}}"></script>
 
 @if (env('APP_ENV') == 'production' && empty(Auth::user()))
