13a52c0a8bbe274f8cbe42f0910fd08c41ccbc8f
Mashpy <mashpysays@gmail.com>
2018-01-15 13:15:19 +0600

    Added migration file

---
+++ database/migrations/2019_01_06_193137_create_email_subscribers_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    //Please active it
+    public function up()
+    {
+        Schema::create('email_subscribers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('email');
+            $table->boolean('subscribe')->default(false);
+            $table->boolean('temporary')->default(false);
+            $table->integer('click_count')->default(0);
+            $table->integer('email_count')->default(0);
+            $table->string('source');
+            $table->dateTime('last_send_email');
+
+            $table->timestamps();
+        });
+
+        Schema::create('email_subscriber_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('email_subscriber_id');
+            $table->integer('category_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_subscribers');
+        Schema::dropIfExists('email_subscribers_categories');
+
+    }
+}
