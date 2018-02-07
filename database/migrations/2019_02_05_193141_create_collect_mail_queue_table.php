566adedea92f926a068ad586f0fbd5597dc3ffd3
Mashpy <mashpysays@gmail.com>
2018-02-07 21:19:53 +0600

    Added collect mail queue

---
+++ database/migrations/2019_02_05_193141_create_collect_mail_queue_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollectMailQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    //Please active it
+    public function up()
+    {
+        Schema::create('collect_mail_queue', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('topic');
+            $table->integer('category_id');
+            $table->string('article_id');
+            $table->text('custom_mail_template');
+            $table->integer('run_count');
+            $table->dateTime('last_time_run');
+            $table->boolean('run_cron_job');
+            $table->timestamps();
+        });
+
+        Schema::table('email_subscribers', function (Blueprint $table) {
+            $table->integer('collect_mail_queue_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collect_mail_queue');
+    }
+}
