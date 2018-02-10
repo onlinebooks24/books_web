ab998787b34de67a05aa0a24ff641e349f9350a6
Mashpy <mashpysays@gmail.com>
2018-02-10 09:52:07 +0600

    Fixed total attempt

--- app/Http/Controllers/Admin/AdminTemporaryEmailController.php
+++ app/Http/Controllers/Admin/AdminTemporaryEmailController.php
@@ -18,77 +18,36 @@ class AdminTemporaryEmailController extends Controller
      */
     public function index(Request $request)
     {
-//        $collect_mail_queues = CollectMailQueue::where('run_cron_job', true)->get();
-//
-//        $one_day = 86400; //second
-//        $total_email_limit = 8000;
-//
-//
-//        foreach($collect_mail_queues as $queue_item){
-//            $count_attempt = $queue_item->limit_cron_job_attemp % 100;
-//                for($i = 0; $i < $count_attempt; $i++){
-//                    $ab = '';
-//                }
-//            }
+        $collect_mail_queues = CollectMailQueue::where('run_cron_job', true)->orderByRaw("RAND()")->get();
 
-        $access_token = env('githubToken');
-        $client = new \GuzzleHttp\Client();
-        $email = [];
-        $username_array = [];
-        $category_id = 5;
+        $total_request_attempt = CollectMailQueue::sum('limit_cron_job_attempt');
 
-        $search_url = $client->get('https://api.github.com/search/code?per_page=20&order=desc&q=tensorflow&sort=indexed&access_token='. $access_token);
-        $event_json =  json_decode($search_url->getBody());
+        $one_day = 86400; //second
+        $total_email_limit = 2;
+//        $average_sleep_time = $one_day/$total_request_attempt;
+        $average_sleep_time = 5;
+        $per_page = 100;
+        $total_attempt = 0;
 
-        foreach($event_json->items as $search_item) {
-            $username = $search_item->repository->owner->login;
+        foreach($collect_mail_queues as $queue_item){
+            $topic = $queue_item->topic;
+            $category_id = $queue_item->category_id;
+            $collect_mail_queue_id = $queue_item->id;
+            $current_attempt_limit = $queue_item->limit_cron_job_attempt;
+            $count_attempt = $queue_item->limit_cron_job_attempt / $per_page;
 
-            if(!in_array($username, $username_array)){
-                $username_array[] = $username;
-                $event_url = $client->get('https://api.github.com/users/'. $username .'/events/public?access_token='. $access_token);
-                $event_json =  json_decode($event_url->getBody());
-                foreach($event_json as $item){
-                    if(isset($item->payload->commits[0])){
-                        $collect_email = $item->payload->commits[0]->author->email;
-
-                        if (!in_array($collect_email, $email) &&
-                            filter_var($collect_email, FILTER_VALIDATE_EMAIL) &&
-                            strpos($collect_email, 'noreply') == false &&
-                            strpos($collect_email, 'local') == false &&
-                            strpos($collect_email, 'internal') == false &&
-                            strpos($collect_email, 'google') == false){
-                            $email[] = $collect_email;
-
-                            $email_subscriber = EmailSubscriber::where('email', $collect_email)->first();
-
-                            if(empty($email_subscriber)){
-                                $email_subscriber = new EmailSubscriber();
-                                $email_subscriber->full_name = null;
-                                $email_subscriber->email = $collect_email;
-                                $email_subscriber->temporary = true;
-                                $email_subscriber->subscribe = true;
-                                $email_subscriber->	source = 'g';  // from our site
-
-                                $email_subscriber->save();
-
-                                $check_already_exist = EmailSubscriberCategory::where('email_subscriber_id', $email_subscriber->id )
-                                    ->where('category_id', $category_id)->first();
-
-                                if(empty($check_already_exist)){
-                                    $email_subscriber_category = new EmailSubscriberCategory();
-                                    $email_subscriber_category->email_subscriber_id = $email_subscriber->id;
-                                    $email_subscriber_category->category_id = $category_id;
-                                    $email_subscriber_category->save();
-                                }
-                            }
+                for($i = 0; $i < $count_attempt; $i++){
+                    if($total_attempt < $total_email_limit){
+                        if($current_attempt_limit > $total_email_limit){
+                            $current_attempt_limit = $total_email_limit;
                         }
+                        $current_attempt_count = $this->ping_github($topic, $category_id, $per_page, $average_sleep_time, $collect_mail_queue_id, $current_attempt_limit);
+                        $total_attempt += $current_attempt_count;
                     }
                 }
-                sleep(1);
             }
-        };
 
-        dd($email);
+        dd($total_attempt);
     }
 
     /**
@@ -157,4 +116,71 @@ class AdminTemporaryEmailController extends Controller
 
     }
 
+    public function ping_github($topic, $category_id, $per_page, $average_sleep_time, $collect_mail_queue_id, $current_attempt_limit){
+        $access_token = env('githubToken');
+        $client = new \GuzzleHttp\Client();
+        $email = [];
+        $username_array = [];
+        $topic = str_replace(' ', '+', $topic);
+        $current_attempt = 0;
+
+        $search_url = $client->get('https://api.github.com/search/code?per_page='. $per_page .'&order=desc&q='. $topic .'&sort=indexed&access_token='. $access_token);
+        $event_json =  json_decode($search_url->getBody());
+
+        foreach($event_json->items as $search_item) {
+            $username = $search_item->repository->owner->login;
+
+            if(!in_array($username, $username_array)){
+                $username_array[] = $username;
+                $event_url = $client->get('https://api.github.com/users/'. $username .'/events/public?access_token='. $access_token);
+                $event_json =  json_decode($event_url->getBody());
+                foreach($event_json as $item){
+                    if(isset($item->payload->commits[0])){
+                        $collect_email = $item->payload->commits[0]->author->email;
+
+                        if (!in_array($collect_email, $email) &&
+                            filter_var($collect_email, FILTER_VALIDATE_EMAIL) &&
+                            strpos($collect_email, 'noreply') == false &&
+                            strpos($collect_email, 'local') == false &&
+                            strpos($collect_email, 'internal') == false &&
+                            strpos($collect_email, 'google') == false){
+                            $email[] = $collect_email;
+
+                            $email_subscriber = EmailSubscriber::where('email', $collect_email)->first();
+
+                            if(empty($email_subscriber)){
+                                $email_subscriber = new EmailSubscriber();
+                                $email_subscriber->full_name = null;
+                                $email_subscriber->email = $collect_email;
+                                $email_subscriber->temporary = true;
+                                $email_subscriber->subscribe = true;
+                                $email_subscriber->	source = 'g';  // from our site
+                                $email_subscriber->collect_mail_queue_id = $collect_mail_queue_id;
+
+                                $email_subscriber->save();
+
+                                $check_already_exist = EmailSubscriberCategory::where('email_subscriber_id', $email_subscriber->id )
+                                    ->where('category_id', $category_id)->first();
+
+                                if(empty($check_already_exist)){
+                                    $email_subscriber_category = new EmailSubscriberCategory();
+                                    $email_subscriber_category->email_subscriber_id = $email_subscriber->id;
+                                    $email_subscriber_category->category_id = $category_id;
+                                    $email_subscriber_category->save();
+                                }
+                                $current_attempt++;
+                            }
+                        }
+                    }
+                }
+                sleep($average_sleep_time);
+            }
+
+            if($current_attempt == $current_attempt_limit ){
+                break;
+            }
+        };
+
+        return $current_attempt;
+    }
 }
