c1038cf5c8bfe804ac2fe0c2c4f88bc942a2124f
Mashpy <mashpysays@gmail.com>
2018-06-27 02:55:55 +0600

    Added another social share

--- resources/views/frontend/articles/show.blade.php
+++ resources/views/frontend/articles/show.blade.php
@@ -224,6 +224,7 @@
 
 @section('run_custom_js_file')
     <script id="dsq-count-scr" src="//onlinebooksreview.disqus.com/count.js" async></script>
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b322acaa95624c1"></script>
     {{--<script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=590d63c61554ce0011357601&product=sticky-share-buttons"></script>--}}
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 @endsection
