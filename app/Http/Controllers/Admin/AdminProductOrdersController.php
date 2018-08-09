4420ea4f567e9c9ffea3108a17dd49a44fdf37f0
Mashpy <mashpysays@gmail.com>
2018-08-09 17:36:16 +0600

    Added excluded method

--- app/Http/Controllers/Admin/AdminProductOrdersController.php
+++ app/Http/Controllers/Admin/AdminProductOrdersController.php
@@ -158,12 +158,19 @@ class AdminProductOrdersController extends Controller
 
         $product_title_array = explode(" ",$product_order->title);
 
+        $exclude_lists = ['the', 'a', 'with', 'to', 'for', 'how', 'in', 'book', ':', 'and', '&'];
+
         $article_array = [];
 
         foreach($product_title_array as $product_title_item){
-            $article = Article::where('slug', 'LIKE', "%$product_title_item%")->pluck('id')->toArray();
-            $article_array = array_unique(array_merge($article_array, $article));
+            if(!array_search(strtolower($product_title_item), $exclude_lists)){
+                $article = Article::where('title', 'LIKE', "% $product_title_item %")->pluck('id')->toArray();
+                $article_array[$product_title_item] = $article;
+//                $article_array = array_unique(array_merge($article_array, $article));
+            }
         }
+
+        dd($article_array);
 
         $suggestion_articles = Article::whereIn('id', $article_array)->get();
         return view('admin.product_orders.edit', compact('product_order', 'suggestion_articles'));
