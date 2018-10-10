5533e015d090bb20f9a0a94ba5e6566a2e4a6138
Mashpy <mashpysays@gmail.com>
2018-10-10 11:27:42 +0600

    Now days ago

--- app/Http/Controllers/Admin/AdminAllReportsController.php
+++ app/Http/Controllers/Admin/AdminAllReportsController.php
@@ -72,7 +72,7 @@ class AdminAllReportsController extends Controller
 
             $articles = Article::where('status', true)->orderBy('created_at', 'desc')->get();
             $last_article = Article::where('status', true)->orderBy('created_at', 'desc')->first();
-            $last_article = $last_article->created_at->diffForHumans();
+            $last_article = ($last_article->created_at->diff(Carbon::now())->days) . ' days ago';
 
             $total_articles = [];
             $individual_articles = [];
