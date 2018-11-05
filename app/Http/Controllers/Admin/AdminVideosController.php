6c06d50702ce30348f04fc187f4bcdb875b0e095
Mashpy <mashpysays@gmail.com>
2018-11-05 22:18:12 +0600

    Fixed ffmpeg video creation problem

--- app/Http/Controllers/Admin/AdminVideosController.php
+++ app/Http/Controllers/Admin/AdminVideosController.php
@@ -192,7 +192,7 @@ class AdminVideosController extends Controller
         }
         //$name="video_".rand(100,100000);
         $name=$this->video_name;
-        shell_exec("ffmpeg -f concat -safe 0 -i '".$this->path."/video_desc.txt' -i '".$this->audio."' -vsync vfr -pix_fmt yuv420p -shortest '".$this->path."/".$name.".mp4'");
+        dd(shell_exec("ffmpeg -f concat -safe 0 -i '".$this->path."/video_desc.txt' -i '".$this->audio."' -vsync vfr -pix_fmt yuv420p  -y -shortest '".$this->path."/".$name.".mp4'"));
 
         return $this->path."/".$name.".mp4";
         
