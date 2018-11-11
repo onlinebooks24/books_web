d3b4661014de86128e181a4b708f2dfd69fae629
Mashpy <mashpysays@gmail.com>
2018-11-11 14:17:04 +0600

    Added video template audio location

--- app/Models/VideosTemplate.php
+++ app/Models/VideosTemplate.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class VideosTemplate extends Model
 {
-    
+    protected $appends = ['background_image_location', 'audio_location'];
+
+    public function getBackgroundImageLocationAttribute(){
+        $background_image_location = public_path('uploads/videos/templates/'. $this->template_name . $this->background_image);
+        return $background_image_location;
+    }
+
+    public function getAudioLocationAttribute(){
+        $audio_location = public_path('uploads/videos/templates/'. $this->template_name . $this->audio_name);
+        return $audio_location;
+    }
 }
