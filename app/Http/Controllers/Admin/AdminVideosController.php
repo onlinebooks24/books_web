7bf86fa9926e58a4251924a438c59947b7cd92da
Mashpy <mashpysays@gmail.com>
2018-11-16 17:23:57 +0600

    Added audio voice on the video

--- app/Http/Controllers/Admin/AdminVideosController.php
+++ app/Http/Controllers/Admin/AdminVideosController.php
@@ -9,6 +9,8 @@ use App\Models\Video;
 use App\Models\Article;
 use Session;
 use Spatie\Browsershot\Browsershot;
+use DOMXPath;
+use DOMDocument;
 
 class AdminVideosController extends Controller
 {
@@ -19,6 +21,7 @@ class AdminVideosController extends Controller
      */
     public function index()
     {
+
         $videos = Video::paginate(10);
         return view('admin.videos.index', compact('videos'));
     }
@@ -68,9 +71,13 @@ class AdminVideosController extends Controller
         }
 
         $video_creator_script = $temp_html_dir . "video_creator_script.txt";
+        $file_desc = fopen($video_creator_script,"w");
 
         $img_no = 1;
-        $file_desc = fopen($video_creator_script,"w");
+
+        $audio_creator_script = $temp_html_dir . "audio_creator_script.txt";
+        $audio_desc = fopen($audio_creator_script,"w");
+
         $audio_location = $video_template->audio_location;
 
         $article = Article::find($request->article_id);
@@ -88,13 +95,79 @@ class AdminVideosController extends Controller
             $new_image_file = $temp_html_dir.$image_name;
 
             Browsershot::url("file://".$temp_html_file)->setScreenshotType('jpeg',100)->windowSize(1280, 720)->save($new_image_file);
+
+            $doc = new DOMDocument();
+            libxml_use_internal_errors(true);
+            $doc->loadHTML($html_item);
+            $finder = new DomXPath($doc);
+            $node = $finder->query("//*[contains(@class, 'video-container')]");
+            $voice_html = $doc->saveHTML($node->item(0));
+            $voice_html = strip_tags($voice_html);
+            $voice_html = str_replace("\n", ".", $voice_html);
+            $voice_html = str_replace("..", ".", $voice_html);
+            $_POST['EID'] = 3;
+            $_POST['LID'] = 1;
+            $_POST['VID'] = 3;
+            $_POST['TXT'] = $voice_html;
+            $_POST['ACC'] = 6887135;
+            $_POST['API'] = 2638673;
+            $_POST['SECRET'] = '589e570dc04cbb23ed03c781c4adeb5d';
+            $_POST['SESSION'] = '';
+
+            $ext = isset($_POST['EXT']) &&
+            in_array(trim(strtolower($_POST['EXT'])), array('mp3','swf')) ?
+                trim(strtolower($_POST['EXT'])) : '';
+            $fxType = isset($_POST['FX_TYPE']) && strlen($_POST['FX_TYPE']) > 0 ?
+                $_POST['FX_TYPE'] : '';
+            $fxLevel= isset($_POST['FX_LEVEL']) && strlen($_POST['FX_LEVEL']) > 0 ?
+                $_POST['FX_LEVEL'] : '';
+            $httpErr= isset($_POST['HTTP_ERR']) && strlen($_POST['HTTP_ERR']) > 0 ?
+                $_POST['HTTP_ERR'] : '';
+//Construct parameters.
+            $get = 'EID='.$_POST['EID']
+                .'&LID='.$_POST['LID']
+                .'&VID='.$_POST['VID']
+                .'&TXT='.urlencode($_POST['TXT'])
+                .'&EXT='.$ext
+                .'&FX_TYPE='.$fxType
+                .'&FX_LEVEL='.$fxLevel
+                .'&ACC='.$_POST['ACC']
+                .'&API='.$_POST['API']
+                .'&SESSION='.$_POST['SESSION']
+                .'&HTTP_ERR='.$httpErr;
+            $CS = md5($_POST['EID'].$_POST['LID'].$_POST['VID'].$_POST['TXT'].
+                $ext.$fxType.$fxLevel.$_POST['ACC']. $_POST['API'].$_POST['SESSION'].
+                $httpErr.$_POST['SECRET']);
+            $url = 'http://www.vocalware.com/tts/gen.php?' . $get . '&CS=' . $CS;
+
+            $new_audio_file = $temp_html_dir. 'audio0000'. $key . '.mp3';
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            $data = curl_exec($ch);
+
+            curl_close($ch);
+
+            file_put_contents($new_audio_file, $data);
+
+            $audio = new \wapmorgan\Mp3Info\Mp3Info($new_audio_file, true);
+
             fwrite($file_desc,"file '".$new_image_file."'\n");
-            fwrite($file_desc,"duration ". $duration_array[$key] ."\n");
+            fwrite($file_desc,"duration ". $audio->duration ."\n");
+
+            fwrite($audio_desc,"file '".$new_audio_file."'\n");
         }
 
         fclose($file_desc);
+        fclose($audio_desc);
 
-        $command = "ffmpeg -f concat -safe 0 -i '".$video_creator_script."' -i ". $audio_location ." -pix_fmt yuv420p -vf scale=1280:720 -y -shortest ".$final_video_path.$video_name;
+        $final_audio = $temp_html_dir .'output.mp3';
+        $command = 'ffmpeg -f concat -safe 0 -i '. $audio_creator_script .' -c copy '. $final_audio;
+
+        shell_exec($command);
+
+        $command = "ffmpeg -f concat -safe 0 -i '".$video_creator_script."' -i ". $final_audio ." -pix_fmt yuv420p -vf scale=1280:720 -y -shortest ".$final_video_path.$video_name;
 
         $video_create_log = shell_exec($command);
 
