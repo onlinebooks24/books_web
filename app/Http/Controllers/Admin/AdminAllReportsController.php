a653e0cceb4a762f045ea7e7be7239e400f606ae
Mashpy <mashpysays@gmail.com>
2019-03-19 19:30:01 +0600

    Fixed report section for subeditor

--- app/Http/Controllers/Admin/AdminAllReportsController.php
+++ app/Http/Controllers/Admin/AdminAllReportsController.php
@@ -21,146 +21,146 @@ class AdminAllReportsController extends Controller
      */
     public function index(Request $request)
     {
-        if(Auth::user()->roleType->name == 'admin'){
-            $product_orders = ProductOrder::all();
+        $role_type_name = Auth::user()->roleType->name;
+        $product_orders = ProductOrder::all();
 
-            $total_sell_from_article = 0;
-            $total_sell_from_non_article = 0;
-            foreach($product_orders as $product_order){
-                $product_order_date = Carbon::parse($product_order->shipment_date)->format('Y_F');
-                if(!isset($monthly_product_sell[(string)$product_order_date])){
-                    $monthly_product_sell[(string)$product_order_date] = 0;
-                }
-                $monthly_product_sell[(string)$product_order_date] += $product_order->ad_fees;
-
-                if(empty($product_order->article_id)){
-                    $total_sell_from_non_article += $product_order->ad_fees;
-                } else {
-                    $total_sell_from_article += $product_order->ad_fees;
-                }
+        $total_sell_from_article = 0;
+        $total_sell_from_non_article = 0;
+        foreach($product_orders as $product_order){
+            $product_order_date = Carbon::parse($product_order->shipment_date)->format('Y_F');
+            if(!isset($monthly_product_sell[(string)$product_order_date])){
+                $monthly_product_sell[(string)$product_order_date] = 0;
             }
+            $monthly_product_sell[(string)$product_order_date] += $product_order->ad_fees;
 
-            $total_whole_sell = $total_sell_from_non_article + $total_sell_from_article;
-
-            $site_costs = SiteCost::all();
-
-            $all_costs = [];
-            $individual_costs = [];
-            $total_costs = 0;
-
-            foreach($site_costs as $key => $site_cost){
-                $cost_type_name = $site_cost->site_cost_type->name;
-                $username = $site_cost->user->name;
-                if(!isset($all_costs[(string)$cost_type_name])){
-                    $all_costs[(string)$cost_type_name] = 0;
-                }
-                $all_costs[(string)$cost_type_name] += $site_cost->amount;
-
-                if(!isset($individual_costs[(string)$username])){
-                    $individual_costs[(string)$username] = 0;
-                }
-
-                $individual_costs[(string)$username] += $site_cost->amount;
-
-                $total_costs += $site_cost->amount;
-
-                $site_cost_date = Carbon::parse($site_cost->when_paid)->format('Y_F');
-                if(!isset($monthly_site_cost[(string)$site_cost_date])){
-                    $monthly_site_cost[(string)$site_cost_date] = 0;
-                }
-                $monthly_site_cost[(string)$site_cost_date] += $site_cost->amount;
+            if(empty($product_order->article_id)){
+                $total_sell_from_non_article += $product_order->ad_fees;
+            } else {
+                $total_sell_from_article += $product_order->ad_fees;
             }
-
-            $articles = Article::where('status', true)->orderBy('created_at', 'desc')->get();
-            $last_article = Article::where('status', true)->orderBy('created_at', 'desc')->first();
-            $last_article = ($last_article->created_at->diff(Carbon::now())->days) . ' days ago';
-
-            $total_articles = [];
-            $individual_articles = [];
-            $individual_cost = [];
-            $individual_revenue = [];
-            $individual_no_sell = [];
-            $article_investment_return = [];
-            $article_view_count = 0;
-            $monthly_article_view_count = [];
-
-            foreach($articles as $article){
-                $article_view_count +=  $article->count;
-                $date = Carbon::parse($article->created_at)->format('Y_F');
-                if(!isset($total_articles[(string)$date])){
-                    $total_articles[(string)$date] = 0;
-                }
-                $total_articles[(string)$date] += 1;
-
-                if(!isset($monthly_article_view_count[(string)$date])){
-                    $monthly_article_view_count[(string)$date] = 0;
-                }
-                $monthly_article_view_count[(string)$date] += $article->count;
-
-                $username = $article->user->name;
-
-                if(!isset($individual_articles[(string)$username])){
-                    $individual_articles[(string)$username] = 0;
-                }
-                $individual_articles[(string)$username] += 1;
-
-                if(!isset($individual_cost[(string)$username])){
-                    $individual_cost[(string)$username] = 0;
-                }
-
-                $cost = 0;
-                if(!empty($article->site_costs)){
-                    foreach($article->site_costs as $item){
-                        $cost += $item->amount;
-                    }
-                }
-
-                $individual_cost[(string)$username] += $cost;
-
-                if(!isset($individual_revenue[(string)$username])){
-                    $individual_revenue[(string)$username] = 0;
-                }
-
-                $fee = 0;
-                if(!empty($article->product_orders)){
-                    foreach($article->product_orders as $item){
-                        $fee += $item->ad_fees;
-                    }
-                }
-
-                $individual_revenue[(string)$username] += $fee;
-
-                if(!isset($article_investment_return[(string)$date])){
-                    $article_investment_return[(string)$date] = 0;
-                }
-
-                $article_investment_return[(string)$date] += $fee;
-
-                if(!isset($individual_no_sell[(string)$username])){
-                    $individual_no_sell[(string)$username] = 0;
-                }
-
-                if($fee == 0){
-                    $individual_no_sell[(string)$username] += 1;
-                }
-
-            }
-
-            $active_writers = User::where(['active_writer' => true])
-                ->get();
-
-
-            return view('admin.all_reports.index',
-                compact('total_sell_from_article',
-                        'site_costs', 'total_sell_from_non_article',
-                        'monthly_product_sell', 'monthly_site_cost',
-                        'total_whole_sell', 'all_costs', 'total_costs',
-                        'articles', 'last_article', 'total_articles',
-                        'individual_costs', 'individual_articles',
-                        'individual_cost', 'individual_revenue',
-                        'individual_no_sell', 'article_investment_return',
-                        'article_view_count', 'monthly_article_view_count', 'active_writers'));
         }
+
+        $total_whole_sell = $total_sell_from_non_article + $total_sell_from_article;
+
+        $site_costs = SiteCost::all();
+
+        $all_costs = [];
+        $individual_costs = [];
+        $total_costs = 0;
+
+        foreach($site_costs as $key => $site_cost){
+            $cost_type_name = $site_cost->site_cost_type->name;
+            $username = $site_cost->user->name;
+            if(!isset($all_costs[(string)$cost_type_name])){
+                $all_costs[(string)$cost_type_name] = 0;
+            }
+            $all_costs[(string)$cost_type_name] += $site_cost->amount;
+
+            if(!isset($individual_costs[(string)$username])){
+                $individual_costs[(string)$username] = 0;
+            }
+
+            $individual_costs[(string)$username] += $site_cost->amount;
+
+            $total_costs += $site_cost->amount;
+
+            $site_cost_date = Carbon::parse($site_cost->when_paid)->format('Y_F');
+            if(!isset($monthly_site_cost[(string)$site_cost_date])){
+                $monthly_site_cost[(string)$site_cost_date] = 0;
+            }
+            $monthly_site_cost[(string)$site_cost_date] += $site_cost->amount;
+        }
+
+        $articles = Article::where('status', true)->orderBy('created_at', 'desc')->get();
+        $last_article = Article::where('status', true)->orderBy('created_at', 'desc')->first();
+        $last_article = ($last_article->created_at->diff(Carbon::now())->days) . ' days ago';
+
+        $total_articles = [];
+        $individual_articles = [];
+        $individual_cost = [];
+        $individual_revenue = [];
+        $individual_no_sell = [];
+        $article_investment_return = [];
+        $article_view_count = 0;
+        $monthly_article_view_count = [];
+
+        foreach($articles as $article){
+            $article_view_count +=  $article->count;
+            $date = Carbon::parse($article->created_at)->format('Y_F');
+            if(!isset($total_articles[(string)$date])){
+                $total_articles[(string)$date] = 0;
+            }
+            $total_articles[(string)$date] += 1;
+
+            if(!isset($monthly_article_view_count[(string)$date])){
+                $monthly_article_view_count[(string)$date] = 0;
+            }
+            $monthly_article_view_count[(string)$date] += $article->count;
+
+            $username = $article->user->name;
+
+            if(!isset($individual_articles[(string)$username])){
+                $individual_articles[(string)$username] = 0;
+            }
+            $individual_articles[(string)$username] += 1;
+
+            if(!isset($individual_cost[(string)$username])){
+                $individual_cost[(string)$username] = 0;
+            }
+
+            $cost = 0;
+            if(!empty($article->site_costs)){
+                foreach($article->site_costs as $item){
+                    $cost += $item->amount;
+                }
+            }
+
+            $individual_cost[(string)$username] += $cost;
+
+            if(!isset($individual_revenue[(string)$username])){
+                $individual_revenue[(string)$username] = 0;
+            }
+
+            $fee = 0;
+            if(!empty($article->product_orders)){
+                foreach($article->product_orders as $item){
+                    $fee += $item->ad_fees;
+                }
+            }
+
+            $individual_revenue[(string)$username] += $fee;
+
+            if(!isset($article_investment_return[(string)$date])){
+                $article_investment_return[(string)$date] = 0;
+            }
+
+            $article_investment_return[(string)$date] += $fee;
+
+            if(!isset($individual_no_sell[(string)$username])){
+                $individual_no_sell[(string)$username] = 0;
+            }
+
+            if($fee == 0){
+                $individual_no_sell[(string)$username] += 1;
+            }
+
+        }
+
+        $active_writers = User::where(['active_writer' => true])
+            ->get();
+
+
+        return view('admin.all_reports.index',
+            compact('total_sell_from_article',
+                    'site_costs', 'total_sell_from_non_article',
+                    'monthly_product_sell', 'monthly_site_cost',
+                    'total_whole_sell', 'all_costs', 'total_costs',
+                    'articles', 'last_article', 'total_articles',
+                    'individual_costs', 'individual_articles',
+                    'individual_cost', 'individual_revenue',
+                    'individual_no_sell', 'article_investment_return',
+                    'article_view_count', 'monthly_article_view_count',
+                    'active_writers', 'role_type_name'));
     }
 
     /**
--- resources/views/admin/all_reports/index.blade.php
+++ resources/views/admin/all_reports/index.blade.php
@@ -45,33 +45,35 @@
                         @endforeach
                     </div>
 
-                    <h5>Sell:</h5>
-                    <div class="alert alert-success">
-                        <div class="bottom5">
-                            Total whole sell: {{ $total_whole_sell * config('constants.dollar_rate') }} BDT
+                    @if($role_type_name == 'admin')
+                        <h5>Sell:</h5>
+                        <div class="alert alert-success">
+                            <div class="bottom5">
+                                Total whole sell: {{ $total_whole_sell * config('constants.dollar_rate') }} BDT
+                            </div>
+                            <span class="btn btn-success">
+                                Total sell from article: {{ $total_sell_from_article * config('constants.dollar_rate') }} BDT
+                            </span>
+                            <span class="btn btn-success">
+                                Total sell from non articles: {{ $total_sell_from_non_article * config('constants.dollar_rate') }} BDT
+                            </span>
                         </div>
-                        <span class="btn btn-success">
-                            Total sell from article: {{ $total_sell_from_article * config('constants.dollar_rate') }} BDT
-                        </span>
-                        <span class="btn btn-success">
-                            Total sell from non articles: {{ $total_sell_from_non_article * config('constants.dollar_rate') }} BDT
-                        </span>
-                    </div>
 
 
-                    <h5>
-                        Cost:
-                    </h5>
-                    <div class="alert alert-danger">
-                        <div class="bottom5">Total Cost: {{ $total_costs }} BDT</div>
-                        @foreach($individual_costs as $key => $value)
-                            <span class="btn btn-danger">{{ $key }} cost: {{ $value }} BDT</span>
+                        <h5>
+                            Cost:
+                        </h5>
+                        <div class="alert alert-danger">
+                            <div class="bottom5">Total Cost: {{ $total_costs }} BDT</div>
+                            @foreach($individual_costs as $key => $value)
+                                <span class="btn btn-danger">{{ $key }} cost: {{ $value }} BDT</span>
+                            @endforeach
+                        </div>
+
+                        @foreach($all_costs as $key=> $value)
+                            <div class="alert alert-danger">{{ $key }} cost: {{ $value }} BDT</div>
                         @endforeach
-                    </div>
-
-                    @foreach($all_costs as $key=> $value)
-                        <div class="alert alert-danger">{{ $key }} cost: {{ $value }} BDT</div>
-                    @endforeach
+                    @endif
                 </div>
                 <div class="col-md-6">
                     <h5>Article publish ratio (Total {{ $articles->count() }}) (View: {{ number_format($article_view_count) }})</h5>
@@ -81,7 +83,8 @@
                                 <div class="alert alert-info">
                                     <div>{{ $key }}</div>
                                     <div class="btn btn-warning top5">Articles: {{ $value }}</div>
-                                    <div class="btn btn-success top5">
+                                    @if($role_type_name == 'admin')
+                                        <div class="btn btn-success top5">
                                         <span>Total sell:</span>
                                         @if(isset($monthly_product_sell[$key]))
                                             <span>{{ $monthly_product_sell[$key] * config('constants.dollar_rate') }} BDT</span>
@@ -89,15 +92,16 @@
                                             <span>0 BDT</span>
                                         @endif
                                     </div>
+                                        <div class="btn btn-info top5">
+                                            <span>Article return:</span>
+                                            @if(isset($article_investment_return[$key]))
+                                                <span>{{ $article_investment_return[$key] * config('constants.dollar_rate') }} BDT</span>
+                                            @else
+                                                <span>0 BDT</span>
+                                            @endif
+                                        </div>
+                                    @endif
 
-                                    <div class="btn btn-info top5">
-                                        <span>Article return:</span>
-                                        @if(isset($article_investment_return[$key]))
-                                            <span>{{ $article_investment_return[$key] * config('constants.dollar_rate') }} BDT</span>
-                                        @else
-                                            <span>0 BDT</span>
-                                        @endif
-                                    </div>
 
                                     <div class="btn btn-default top5">
                                         <span>Article View Count:</span>
@@ -108,7 +112,8 @@
                                         @endif
                                     </div>
 
-                                    <div class="btn btn-danger top5">
+                                    @if($role_type_name == 'admin')
+                                        <div class="btn btn-danger top5">
                                         <span>Total cost:</span>
                                         @if(isset($monthly_site_cost[$key]))
                                             <span>{{ $monthly_site_cost[$key] }} BDT</span>
@@ -116,6 +121,7 @@
                                             <span>0 BDT</span>
                                         @endif
                                     </div>
+                                    @endif
                                 </div>
                             </div>
                         @endforeach
@@ -133,12 +139,14 @@
                                     <div>
                                         No Selling Article: {{ $individual_no_sell[$key] }}
                                     </div>
-                                    <div>
-                                        Total Cost: {{ $individual_cost[$key] }}
-                                    </div>
-                                    <div>
-                                        Total Revenue: {{ $individual_revenue[$key] * config('constants.dollar_rate') }}
-                                    </div>
+                                    @if($role_type_name == 'admin')
+                                        <div>
+                                            Total Cost: {{ $individual_cost[$key] }}
+                                        </div>
+                                        <div>
+                                            Total Revenue: {{ $individual_revenue[$key] * config('constants.dollar_rate') }}
+                                        </div>
+                                    @endif
                                 </div>
                             </div>
                         @endforeach
