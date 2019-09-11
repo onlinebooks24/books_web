60c9451900c88a7b338fb8d0edcb4e9d400340ee
Mashpy <mashpysays@gmail.com>
2019-09-11 11:20:02 +0600

    Fixed scholarship page css problem

--- resources/views/frontend/other/scholarship.blade.php
+++ resources/views/frontend/other/scholarship.blade.php
@@ -7,7 +7,7 @@
     @include('includes.header')
 <div class="container">
     <div class="row">
-        <div class="col-sm-12" class="text-center">
+        <div class="col-sm-12">
             <p><strong>&nbsp;</strong></p>
             <p><a href="https://www.onlinebooksreview.com/">OnlineBooksReview</a> is the Market Leader on reviewing different kinds of&nbsp;online books&nbsp;including&nbsp;<a href="https://www.onlinebooksreview.com/articles/best-mcat-test-books-2019">best mcat prep books</a>,&nbsp;<a href="https://www.onlinebooksreview.com/articles/comptia-a+-certification-book">comptia a+ book</a>,&nbsp;<a href="https://www.onlinebooksreview.com/articles/best-geometry-high-school-book">geometry book</a>,&nbsp;<a href="https://www.onlinebooksreview.com/articles/best-calligraphy-book">calligraphy books </a>etc.</p>
         <h2><strong>Background on Scholarship</strong></h2>
@@ -15,36 +15,34 @@
         
         <h2><strong>Eligibility</strong></h2>
       <div>
-        <ul class="a">
-        <li>Students must be attending a two or four year university or college in 2019.</li>
-        <li>OnlineBooksReview&nbsp;employees and their family members are not eligible to apply for this scholarship.</li>
-        <li>Submission of application is regarded as acknowledgement and adherence to the following agreement guidelines below.</li>
+        <ul>
+            <li>1.Students must be attending a two or four year university or college in 2019.</li>
+            <li>2. OnlineBooksReview&nbsp;employees and their family members are not eligible to apply for this scholarship.</li>
+            <li>3. Submission of application is regarded as acknowledgement and adherence to the following agreement guidelines below.</li>
         </ul>
     </div>
         <p><strong>&nbsp;</strong></p>
         <h2><strong>Application Requirements</strong></h2>
         <div >
-        <ul class="b">
-            <li>Each applicant must submit an essay of 700-800 words which includes</li>
-            <li>Full name</li>
-
-            <li>Mailing address and street address, if different</li>
-            <li>Phone number</li>
-            <li>Email address</li>
-            <li>Area of study</li>
-            <li>Name and mailing address of the university or college accepted into 2019</li>
-            <li>Anticipated date of university or college graduation</li>
-            <li>Your current GPA (high school or university/college)</li>
+        <ul>
+            <li>1. Each applicant must submit an essay of 700-800 words which includes</li>
+            <li>2. Full name</li>
+            <li>3. Mailing address and street address, if different</li>
+            <li>4. Phone number</li>
+            <li>5. Email address</li>
+            <li>6. Area of study</li>
+            <li>7. Name and mailing address of the university or college accepted into 2019</li>
+            <li>8. Anticipated date of university or college graduation</li>
+            <li>9. Your current GPA (high school or university/college)</li>
         </ul>
 
         <p><strong>&nbsp;</strong></p>
-        <p><strong>&nbsp;</strong></p>
-        <h3 class="text-center"><strong>Scholarship Topic</strong></h3>
-        <p class="text-center">Write a short essay (700-800 words) describing &ldquo;Why are Ebooks Better Than Regular Books?&rdquo;</p>
-        <h2 class="text-center"><a href="https://www.onlinebooksreview.com/scholarship/"><strong>Why?</strong></a></h2>
-        <h4 class="text-center">Submission Must Be In Either PDF or Word Format</h4>
-        <div class="text-center">
-            <p>When you will be done with these steps, email us at&nbsp;scholarships@onlinebooksreview.com&nbsp;with all of the details listed below and attached Word document.</p>
+        <h3><strong>Scholarship Topic</strong></h3>
+        <p>Write a short essay (700-800 words) describing &ldquo;Why are Ebooks Better Than Regular Books?&rdquo;</p>
+        <h3><a href="https://www.onlinebooksreview.com/scholarship/"><strong>Why?</strong></a></h3>
+        <h4>Submission Must Be In Either PDF or Word Format</h4>
+        <div>
+            <p>When you will be done with these steps, email us at&nbsp;info@onlinebooksreview.com&nbsp;with all of the details listed below and attached Word document.</p>
         <p><strong>Application Submission Deadline:</strong>&nbsp;December 10, 2019</p>
         <p><strong>Award:</strong>&nbsp;One student will be awarded a $500 scholarship</p>
         <p><strong>Award Date:</strong>&nbsp;September 2019</p>
